bb2512e13b152b868b13d3f0ac16acb65eefe664
BL <BrendanL72@users.noreply.github.com>
2022-11-01 11:07:43 -0500

    Modified gitignore and removed \r chars

--- OCSVM/src/run_svm.sh
+++ OCSVM/src/run_svm.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-
-data_dir=../data 
-output_dir=../output
+data_dir=./data
+output_dir=./output
 
 mkdir -p ${output_dir}
-echo "Training..."
-./OCSVM/svm-train -s 2 -b 1 -n 0.25 -g 0.0001 -h 0 ${data_dir}/freq_train > ${output_dir}/training_output
 
-echo "Predicting..."
-./OCSVM/svm-predict -b 1 ${data_dir}/freq_test ./freq_train.model ${output_dir}/test_output
+echo "Training..." 
+./src/OCSVM/svm-train -s 2 -b 1 -n 0.25 -g 0.0001 -h 0 ${data_dir}/freq_train > ${output_dir}/training_output
+
+echo "Predicting..." 
+./src/OCSVM/svm-predict -b 1 ${data_dir}/freq_test ./freq_train.model ${output_dir}/test_output
