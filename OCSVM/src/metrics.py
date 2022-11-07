8b203f6dde93a60f3103d9e2f80d9bfbc8649af5
BL <BrendanL72@users.noreply.github.com>
2022-11-07 00:37:52 -0600

    Added positivity percentage to metrics

--- OCSVM/src/metrics.py
+++ OCSVM/src/metrics.py
@@ -5,6 +5,8 @@ import numpy as np
 import pandas as pd
 from sklearn.metrics import recall_score, f1_score, precision_score
 
+
+#file names and paths
 output_path = "./output"
 
 gamma_preds_file_name = "gamma_preds"
@@ -13,15 +15,27 @@ gamma_preds_path = Path(output_path, gamma_preds_file_name)
 test_data_file_name = "true_test_values"
 test_output_path = Path(output_path , test_data_file_name)
 
+
 #read files and load into arrays
 pred_outcomes = np.loadtxt(gamma_preds_path).astype(bool)
-
 actual_outcomes = np.loadtxt(test_output_path).astype(bool)
 
-print(pred_outcomes)
-print(actual_outcomes)
+
+#numpy wizardy lets you find number of bools based on sum of bools
+num_gauss_preds = pred_outcomes.sum()
+real_positives = actual_outcomes.sum()
+
+gauss_pred_pos_percent = num_gauss_preds/pred_outcomes.shape[0]
+real_pos_percentage = real_positives/actual_outcomes.shape[0]
+
 
 #find positivity rate for prediction and actual outcomes
+print("Number of Predicted Positives:", num_gauss_preds)
+print("Actual Positives in Dataset:", real_positives)
+
+print("Percentage of Predicted Positives:",gauss_pred_pos_percent)
+print("Percentage of Actual Positives in Test Dataset", real_pos_percentage)
+
 
 #compute anomaly detection metrics: precision, recall, and f1 score
 prec_score = precision_score(actual_outcomes, pred_outcomes, average = "binary")
