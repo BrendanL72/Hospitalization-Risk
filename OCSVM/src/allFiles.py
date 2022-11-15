b4f594c7d2d2f2051d910a14dd6b85cc9c6ea54a
BrendanL <BrendanL72@users.noreply.github.com>
2022-11-14 23:09:30 -0600

    Changed allFiles to work properly
    also tried out weighting

--- OCSVM/src/allFiles.py
+++
@@ -1,5 +0,0 @@
-## This file will run all the code in order
-
-exec(open("./data_prep.py").read()) ## runs the first file
-exec(open("./metrics.py.py").read()) ## runs the next file 
-exec(open("./new_gaussian_robs.py").read()) ## runs the last file
